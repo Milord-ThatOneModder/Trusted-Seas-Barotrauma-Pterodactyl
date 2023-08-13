9fc5285da5fe90b3c7566aa41352b3d84b949253
Milord-ThatOneModder <100714051+Milord-ThatOneModder@users.noreply.github.com>
2023-08-13 19:55:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,7 +107,16 @@ MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # ModManager adding
-./mod_manager.sh
+modmanager_install_script="#!/bin/bash
+
+echo \"UPDATING MOD MANAGER\"
+wget -N https://github.com/Milord-ThatOneModder/Barotrauma-ModManager/releases/latest/download/ModManager.zip
+unzip -qo ModManager.zip
+echo -e \"\\n\\n\"
+echo \"RUNNING MOD MANAGER\"
+python3 ModManager/ModManager.py -s \"steamcmd/steamcmd.sh\" -t \"ModManager\""
+echo "$modmanager_install_script" >> custom_script.sh
+chmod +x custom_script.sh
 
 touch custom_script.sh
 chmod +x custom_script.sh
